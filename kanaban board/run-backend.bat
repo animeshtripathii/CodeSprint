e529e5dc9df62f7666018f701b91bc174dfa1da2
Animesh Tripathi <tripathianimesh38@gmail.com>
2026-08-02 16:56:35 +0530

    Delete kanaban board directory

--- kanaban board/run-backend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Kanban Backend Server
-cd server
-echo Installing server dependencies...
-call npm install
-echo Starting backend server on http://localhost:5000...
-npm run dev
-pause
